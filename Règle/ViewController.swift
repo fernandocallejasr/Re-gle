e7b335d58bbe83a83b492d6e25a26e1f0d1705ba
Fernando Callejas <ferd.callejas@gmail.com>
2020-08-31 22:18:26 -0500

    Restart Measurements
    Added functionality that allows to start a new measurement after selecting a third point in space, deleting previous 2 points and the 3D Text

--- Règle/ViewController.swift
+++ Règle/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     
     var nodesArray = [SCNNode]()
+    var textNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +44,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
         
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if nodesArray.count >= 2 {
+            for node in nodesArray {
+                node.removeFromParentNode()
+            }
+            nodesArray = [SCNNode]()
+        }
         
         if let touchLocation = touches.first?.location(in: sceneView) {
             
@@ -98,10 +106,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func updateText(text: String, atPosition position: SCNVector3) {
         
+        textNode.removeFromParentNode()
+        
         let textGeometry = SCNText(string: "\(text) m", extrusionDepth: 1.0)
         textGeometry.firstMaterial?.diffuse.contents = UIColor.red
         
-        let textNode = SCNNode(geometry: textGeometry)
+        textNode = SCNNode(geometry: textGeometry)
         textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
         textNode.scale = SCNVector3(0.01, 0.01, 0.01)
         
